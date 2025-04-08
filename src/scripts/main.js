d5b40a3c682b41e750b4131490a8f222af4cae73
Oleh Havrylyuk <edifier373@gmail.com>
2025-04-08 13:42:30 +0300

    second solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const forms = document.querySelectorAll('form');
 
 forms.forEach((form) => {
-  const inputs = forms.querySelectorAll('input');
+  const inputs = form.querySelectorAll('input');
 
   inputs.forEach((input) => {
     const label = document.createElement('label');
